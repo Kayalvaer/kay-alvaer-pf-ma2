769ca1b18522d70ecc1b86febd29dd494bb16c47
Kayalvaer <62592167+Kayalvaer@users.noreply.github.com>
2020-05-31 20:33:20 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,73 @@
+// question 1
+
+// Answer for question 1 
+var petArray ["cat", "cow", "dog"];
+
+
+// question 2
+
+// Answer for question 2 
+petArray [1] 
+
+// question 3
+
+// Answer for question 3
+var petArray = ["cat", "cow", "dog"];
+petArray.push("sheep");
+// question 4
+
+// Answer for question 4
+var petArray = ["cat", "cow", "dog", "sheep"];
+var petArray = petArray.length;
+console.log (petArrayItems);
+ //or
+ var petArray = ["cat", "cow", "dog", "sheep"];
+ for (petArray i=0; i<petArray.length; i++); {
+     console.log(petArray [1]);
+ }
+
+// question 5
+
+// Answer for question 5
+var catObject = {
+    name="Teddy",
+    color="Black",
+    age=3
+};
+
+
+// question 6
+
+// Answer for question 6
+console.log (catObject.age);
+
+
+// question 7
+
+// Answer for question 7
+var catArray = [{"Teddy", "Black", 3}]
+
+// question 8
+
+// Answer for question 8
+for (var i=o; i<catArray.length; i++){
+    console.log(catArray[0].name);
+    console.log(catArray[1].color);
+    console.log(catArray[2].age);
+}
+
+// question 9
+
+// Answer for question 9
+function logToConsole(theLog){
+    console.log(theLog);
+}
+logToConsole();
+
+// question 10
+
+// Answer for question 10
+function logToConsole(theLog){
+    console.log(theLog);
+}
+logToConsole("donkey");
